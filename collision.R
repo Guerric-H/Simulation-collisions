4e13582ceedc8d75bf337dfe36a02d25791f2ad4
Guerric Herve <guerric.herve@gmail.com>
2022-04-13 10:09:57 +0200

    prettier collision and e2_90succes.R

--- collision.R
+++ collision.R
@@ -7,13 +7,14 @@ proba = V1
 moy_etat = V2
 
 plot(proba, 
-    type = "l", 
-    lty = 3,
-    lwd = 2,
+     type = "p",
+    pch = 21,
+    cex = 0.3,
     xlab = "K (nombre de capteurs actifs)", 
     ylab = "Probabilité de collision",
     main = "Probabilité de collision moyenne pour K capteurs", 
     col = "red", 
+    bg = "red",
     ylim = c(0,1))
 abline(h=0.7,col="blue")
 abline(v=60,col="grey",lty = 5, lwd = 1)
--- e2_90succes.R
+++ e2_90succes.R
@@ -4,15 +4,15 @@ attach(data)
 pdf(file="e2_90succes.pdf")
 
 moy_etat = V2
-
-plot(moy_etat, 
-    type = "l",
-    lty = 3, 
-    lwd = 2,
+plot(moy_etat,
+    type = "p",
+    pch = 21,
+    cex = 0.3,
     xlab = "K (nombre de capteurs actifs)", 
     ylab = "Probabilité de succès de e1 et e2 sur le total de transmission",
     main = "Probabilité de succès de transmission en au plus 2 tentatives \n en fonction du nombre de capteurs", 
     col = "red", 
+    bg = "red",
     ylim = c(0,1))
 abline(h=0.9,col="blue")
 abline(v= 20,lty = 5, lwd = 1, col="grey")
